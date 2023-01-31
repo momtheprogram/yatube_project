937e859cc89d4e562f966fd4f27126d90331fc1f
Наталья Ляховицкая <momtheprogram@gmail.com>
2023-01-31 15:37:07 +0300

    Добавлены urls и view-функции

---
+++ yatube/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    # Страница сгрупповыми постами
+    path('posts/', views.group_posts),
+    # Отдельная страница с полным текстом поста
+    path('posts/<slug:slug>/', views.posts_detail),
+] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request):
+    return HttpResponse('Групповые посты')
+
+
+# Страница с полным текстом поста;
+# view-функция принимает параметр pk из path()
+def posts_detail(request, pk):
+    return HttpResponse(f'Полный текст поста {pk}') 
